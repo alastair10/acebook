4aa77da74c002fbd11fa3320db7af94484204eeb
adonl <adnlwly@gmail.com>
2022-12-06 16:36:32 +0000

    finish frontend integration

--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
 
 const Profile = ({ navigate }) => {
 
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [user, setUser] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userHomeTown, setUserHomeTown] = useState('');
+  const [userBio, setUserBio] = useState('');
+  const [userBirthday, setUserBirthday] = useState('');
+  const [userRelationshipStatus, setUserRelationshipStatus] = useState('');
+  const [userOccupation, setUserOccupation] = useState('');
+  const [userJoinedDate, setUserJoinedDate] = useState('');
+  const { id } = useParams();
 
-  fetch("/users/:id", {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  }).then(response => response.json())
-    .then(async data => {
-      setUser(data.user)
-    })
+  useEffect(() => {
+    fetch("/users/" + id, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(response => response.json())
+      .then((data) => {
+        setUserName(data.full_name)
+        setUserHomeTown(data.hometown)
+        setUserBio(data.bio)
+        setUserBirthday(data.birthday)
+        setUserRelationshipStatus(data.relationship_status)
+        setUserOccupation(data.occupation)
+        setUserJoinedDate(data.joined)
+      })
+  })
 
-console.log(user)
+
+
 
   return (
     <div className="container">
-      <h2>'s Profile</h2>
+      <h2>{userName}'s Profile</h2>
 
-
+      <p>Hometown: {userHomeTown}</p>
+      <p>Bio: {userBio}</p>
+      <p>Birthday: {userBirthday}</p>
+      <p>Occupation: {userOccupation}</p>
+      <p>Relationship Status: {userRelationshipStatus}</p>
+      <p>Join date: {userJoinedDate}</p>
     </div>
   );
 } 
