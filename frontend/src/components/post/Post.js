579f991734045156132e99f427b09b9a03e01a6f
Joseph Lander <josephclander@gmail.com>
2022-12-07 15:49:58 +0000

    Add toggle for like button click

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,36 +1,24 @@
-import React, { useState } from 'react';
-import './Post.css';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import React, { useState } from "react";
+import "./Post.css";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const Post = ({ post, callback }) => {
   const [details, setDetails] = useState(false);
-  // const [likes, toggleLikes] = useState(false);
-  /*in CSS we have 2 classes to button, and here we can
-   *change class every time then click the button
-   */
-  const btnClassName = details ? 'post-full-text' : 'post-less-text';
+  const [isLiked, toggleIsLiked] = useState(false);
 
-  // postman for making posts
-
-  // create a user email and password
-  // returns user_id and token (e.g.something like 234t514tq4t.4tq4t3q4tqqt.q4tqwt3t4qwt)
-
-  // then login with these details
-
-  // POST /posts send req.body {message: ?}
-  // returns {message: ok, token: token}
+  const btnClassName = details ? "post-full-text" : "post-less-text";
 
   const handleClick = async () => {
-    // If there's a token, fetches posts with token for authorization
-    const user_id = window.localStorage.getItem('user_id');
-    const token = window.localStorage.getItem('token');
+    toggleIsLiked((prevState => !prevState));
+
+    const user_id = window.localStorage.getItem("user_id");
+    const token = window.localStorage.getItem("token");
 
     if (user_id) {
-      console.log(post._id);
       let response = await fetch(`/posts/${post._id}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({ likes: user_id }),
@@ -47,47 +35,49 @@ const Post = ({ post, callback }) => {
   };
 
   return (
-    <div className='post'>
-      {/* post section start
-       * each post inclide:
-       * header: user icon, user name and timestamp,
-       * body: article with post text,
-       * footer: likes and comments buttons
-       * comments block after footer
-       */}
-      <div className='post-header'>
+    <div className="post">
+      <div className="post-header">
         <img
-          className='user-icon'
-          alt='user-icon'
-          src='./avatars/avatar_1.png'
+          className="user-icon"
+          alt="user-icon"
+          src="./avatars/avatar_1.png"
         />
-        {/* <img className='user-icon' alt="user-icon" src='./user-icon.png'/> */}
-        <div className='post-header-info'>
+        <div className="post-header-info">
           <p>User Name</p>
-          {/* npm package used to format the date/time */}
           <label>
             {formatDistanceToNow(new Date(post.createdAt), { addSuffix: true })}
           </label>
         </div>
       </div>
-      <div className='post-body'>
-        <article className={btnClassName} data-cy='post' key={post._id}>
+      <div className="post-body">
+        <article className={btnClassName} data-cy="post" key={post._id}>
           {post.message}
         </article>
       </div>
-      <div className='post-footer'>
-        <span>Likes: {post.likes.length} </span>
-        <button className='btn-details' onClick={handleClick}>
-          Like
+      <div className="post-footer">
+        <button className="btn-details" onClick={handleClick}>
+          {isLiked ? (
+            <img
+              className="img_likes"
+              src="../heart_full.svg"
+              alt="full heart"
+            />
+          ) : (
+            <img
+              className="img_likes"
+              src="../heart_empty.svg"
+              alt="empty heart"
+            />
+          )}
         </button>
+        <span>{post.likes.length} </span>
 
-        {/* .show button only if length more then 4 lines of text */}
         {post.message.length > 390 && (
           <button
-            className='btn-details'
+            className="btn-details"
             onClick={() => setDetails((prev) => !prev)}
           >
-            {details ? 'Show less...' : 'Show more...'}
+            {details ? "Show less..." : "Show more..."}
           </button>
         )}
       </div>
